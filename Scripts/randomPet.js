6eda1b3ddab7222dbc2dda8fcf143b6ab021e989
spamalope01 <spamalope01@yahoo.com>
2016-09-22 18:47:57 -0700

    got the modal to display!  now i have to hook up the handlebars and show the corresponding pet details.

--- Scripts/randomPet.js
+++ Scripts/randomPet.js
@@ -27,30 +27,24 @@
       var html    = template(e);
       $('#random').append(html);
     });
-    var modal = document.getElementById('randomModal');
-
-// Get the button that opens the modal
-var btn = document.getElementByClass("modalBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeModal")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    };
+    $('.modalBtn').off().on('click', function() {
+      $('div.modal').toggleClass('modal-show');
+    });
+    $('.closeModal').off().on('click', function() {
+      $('div.modal').toggleClass('modal-show');
+    });
+  };
 
 // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // };
+
+
+
+
     // $('.modalBtn').off().on('click', function(){
     //   console.log(modal);
     //   $('#randomModal').style.display = "block";
@@ -65,7 +59,6 @@ btn.onclick = function() {
       // console.log(randomPets.randomWanted);
       // randomPets.randomDetails(randomPets.randomWanted);
     // });
-  };
 
   //have to launch the modal
   // randomPets.randomDetails = function(petID) {

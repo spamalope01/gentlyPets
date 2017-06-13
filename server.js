fe9f7700c312c6d12ec2fd22bab9c60461cf8c38
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-06-13 12:57:41 -0700

    working on deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/Scripts'));
+app.use(express.static(__dirname + '/app'));
 
 
 app.get('/', function(req, res) {
